f509bac4960db96a3f42e37dfe2d7ad66e3673ff
Rico Putra Anugerah <93566840+ricogann@users.noreply.github.com>
2022-11-04 20:44:19 +0700

    auth admin

--- src/router/authAdmin.router.js
+++ src/router/authAdmin.router.js
@@ -3,8 +3,17 @@ const router = express.Router();
 
 import authAdminController from "../controller/authAdmin.controller.js";
 
-router.post("/login", async (req, res) => {
-    await authAdminController.loginAdmin(req, res);
+router.post("/login", async (req, res, next) => {
+    console.log(req.body);
+    await authAdminController.loginAdmin(req, res, next);
 });
+
+router.get(
+    "/logout",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res, next) => {
+        await authAdminController.logoutAdmin(req, res, next);
+    }
+);
 
 export default router;
